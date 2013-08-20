8b4a339bcb071e07767bd1e8ba696f676cd19b9d
jolth <jolthgs@gmail.com>
2013-08-20 17:33:03 -0500

    not found and menu

--- code.py
+++ code.py
@@ -8,20 +8,37 @@
 """
 import web
 from urls import urls
-from view import render
+from view import render, menu
+
+#app = web.application(urls, globals())
+
+#render = web.template.render("templates")
+    
+# Custom NotFound message
+def notfound():
+    #return web.notfound("Lo sentimos, la página que buscas no se ha encontrado.")
+    return web.notfound(render.notfound())
+
+#app.notfound = notfound
 
 
 class index:
     def GET (self):
-        return render.base(render.index())
+        print web.ctx.path
+        #return render.base(render.index())
+        #return render.base(menu(iweb.ctx.path), render.index())
+        return render.base(menu, web.ctx.path, render.index())
 
 class contact:
     def GET (self):
         #return render.base("Contact Us", "Contáctenos")
-        return render.base(render.contact("Contáctenos"), "Contáctenos")
+        print web.ctx.path
+        #return render.base(menu(), render.contact("Contáctenos"), "Contáctenos")
+        return render.base(menu, web.ctx.path, render.contact("Contáctenos"), "Contáctenos")
 
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     #app.internalerror = web.debugerror
+    app.notfound = notfound
     app.run()
--- view.py
+++ view.py
@@ -1,3 +1,33 @@
+# -*- coding: utf-8 -*-
 import web
 
 render = web.template.render("templates/")
+
+
+#def menu (item=None):
+def menu ():
+    #items = {
+    #    "Home":"/",
+    #    "Soluciones":"/solutions",
+    #    "Servicios":"/services",
+    #    "Productos":"/products",
+        #"Conocimiento":"",
+        #"Formación":"/training",
+    #    "Formacion":"/training",
+    #    "Sobre Dev":"/about",
+        #"Contáctenos":"/contact"
+    #    "Contactenos":"/contact"
+    #}
+    items = [
+        ("Home","/",1),
+        ("Soluciones","/solutions",2),
+        ("Servicios","/services",3),
+        ("Productos","/products",4),
+        #"Conocimiento":"",
+        ("Formación","/training",5),
+        #("Sobre Dev","/about",6),
+        ("Dev","/about",6),
+        ("Contáctenos","/contact",7)
+    ]
+    #return items
+    return sorted(items, key=lambda item: item[2]) # sort by num
